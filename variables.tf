75de79304ae2958409c6ee219dbd03f6f8a80075
freelandr <richard.freeland@atos.net>
2022-11-24 08:54:08 +0000

    initial commit

---
+++ variables.tf
@@ -0,0 +1,20 @@
+variable "profile" {
+  type    = string
+  default = "default"
+}
+
+variable "region-master" {
+  type    = string
+  default = "us-east-1"
+}
+
+variable "region-worker" {
+  type    = string
+  default = "us-west-2"
+}
+
+variable "external_ip" {
+  type        = string
+  description = "The public IP address of the admin / terminal server"
+  default     = "0.0.0.0/0"
+}
